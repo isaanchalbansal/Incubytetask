1020b60611b9a450eb609dab5d08227884a39476
isaanchalbansal <aanchal.bansal141@gmail.com>
2021-10-22 12:54:33 +0530

    Task1- added code to add two no. separated by ,

--- codefortask/src/codeinjava/StringCalc.java
+++ codefortask/src/codeinjava/StringCalc.java
@@ -5,6 +5,10 @@ public class StringCalc {
         if(numbers.equals("")){
             return 0;
         }
+        else if(numbers.contains(",")){
+            String[] num = numbers.split(",");
+            return Integer.parseInt(num[0]) + Integer.parseInt(num[1]);
+        }
         else
             return 1;
     }    
