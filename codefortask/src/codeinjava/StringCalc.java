0909b0f0d1251065a25b0a3858298ba2aae88488
isaanchalbansal <aanchal.bansal141@gmail.com>
2021-10-22 12:48:12 +0530

    Task1- included code for handling one number

--- codefortask/src/codeinjava/StringCalc.java
+++ codefortask/src/codeinjava/StringCalc.java
@@ -2,6 +2,10 @@ package codeinjava;
 
 public class StringCalc {
     public static int add(String numbers){
+        if(numbers.equals("")){
             return 0;
+        }
+        else
+            return 1;
     }    
 }
