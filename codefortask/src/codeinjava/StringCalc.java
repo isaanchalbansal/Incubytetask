d50f741ca62f1d8891b707413fdfdbd8e482f601
isaanchalbansal <aanchal.bansal141@gmail.com>
2021-10-22 16:35:02 +0530

    Task 3- added code to handle new line

--- codefortask/src/codeinjava/StringCalc.java
+++ codefortask/src/codeinjava/StringCalc.java
@@ -5,16 +5,16 @@ public class StringCalc {
         if(numbers.equals("")){
             return 0;
         }
-        else if(numbers.contains(",")){
-            return sum(splitNumbers(numbers));
-        }
-        else
-            return 1;
+        else{
+		    numbers.replaceAll("\n", ",");
+			String numList[] = splitNumbers(numbers, ",");
+			numList= splitNumbers(numbers, ",|\n");
+			return sum(numList);
+		}
     }
-        
-    private static String[] splitNumbers(String num){
-        return num.split(",");
-    }
+    private static String[] splitNumbers(String numbers, String delim){
+	    return numbers.split(delim);
+	}
         
     private static int sum(String[] num){
         int sum = 0;
