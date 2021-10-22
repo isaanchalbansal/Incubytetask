03a3f48e6ab9656495b4de8cccd6fa1bf1663939
isaanchalbansal <aanchal.bansal141@gmail.com>
2021-10-22 13:49:30 +0530

    Task2- refactor code

--- codefortask/src/codeinjava/StringCalc.java
+++ codefortask/src/codeinjava/StringCalc.java
@@ -6,16 +6,24 @@ public class StringCalc {
             return 0;
         }
         else if(numbers.contains(",")){
-            String[] num = numbers.split(",");
-            int sum = 0;
-            for(String eachnum : num){
-                sum += convertToInt(eachnum);
-            }
-            return sum;
+            return sum(splitNumbers(numbers));
         }
-        else 
-            return 1; 
+        else
+            return 1;
     }
+        
+    private static String[] splitNumbers(String num){
+        return num.split(",");
+    }
+        
+    private static int sum(String[] num){
+        int sum = 0;
+        for(String eachnum : num){
+            sum += convertToInt(eachnum);
+        }
+        return sum;
+    }
+    
     private static int convertToInt(String nom){
         return Integer.parseInt(nom);
     }   
